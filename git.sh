464d1cf5c9562689d145214c1281552aeb50c783
Adam Rush <adamrushuk@gmail.com>
2022-06-25 15:00:26 +0100

    Fixed git installer

--- git.sh
+++ git.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
 DIR=$(cd "$(dirname "$0")" && pwd)
 source "$DIR/.lib.sh"
 
 # show available versions:
 # apt-cache madison git | head -n 40
 # https://github.com/git/git/tags
-GIT_VERSION="1:2.35.1-0ppa1~ubuntu18.04.1"
+GIT_VERSION="1:2.34.1-1ubuntu1.2"
 # https://github.com/GitCredentialManager/git-credential-manager/releases
-GCM_VERSION="2.0.632"
+GCM_VERSION="2.0.696"
 
 start "Git $GIT_VERSION"
 start "Git Credential Manager $GCM_VERSION"
@@ -21,7 +22,7 @@ sudo apt-get install git=$GIT_VERSION
 # https://github.com/GitCredentialManager/git-credential-manager#ubuntudebian-distributions
 # download binary
 GCM_DOWNLOAD_PATH="/tmp/gcmcore-linux_amd64.deb"
-curl -L -o $GCM_DOWNLOAD_PATH https://github.com/GitCredentialManager/git-credential-manager/releases/download/v${GCM_VERSION}/gcmcore-linux_amd64.${GCM_VERSION}.34631.deb
+curl -L -o $GCM_DOWNLOAD_PATH https://github.com/GitCredentialManager/git-credential-manager/releases/download/v${GCM_VERSION}/gcmcore-linux_amd64.${GCM_VERSION}.deb
 
 # install
 sudo dpkg -i $GCM_DOWNLOAD_PATH
