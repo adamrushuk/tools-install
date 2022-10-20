9612c4252e2adc81c368a31381b75f316d2fe2c2
Adam Rush <adamrushuk@gmail.com>
2022-10-20 16:45:08 +0100

    Bumped packer version

--- packer.sh
+++ packer.sh
@@ -5,7 +5,9 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 source "$DIR/.lib.sh"
 
 # https://github.com/hashicorp/packer/releases
-VERSION="1.8.3"
+# apt-cache madison packer | head -n 40
+# apt list -a packer
+VERSION="1.8.3-1"
 
 start "Packer $VERSION"
 
@@ -13,6 +15,7 @@ start "Packer $VERSION"
 # https://www.packer.io/downloads
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install packer
+sudo apt update
+sudo apt install packer=$VERSION
 
 end 'packer' 'version'
