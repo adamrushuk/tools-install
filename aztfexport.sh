f52c9f7adc1814c99eb42b4b9bc1bf8d24fcd485
Adam Rush <adamrushuk@gmail.com>
2023-03-07 08:57:34 +0000

    Changed aztfy to aztfexport

---
+++ aztfexport.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
+DIR=$(cd "$(dirname "$0")" && pwd)
+source "$DIR/.lib.sh"
+
+# https://github.com/Azure/aztfexport/releases
+ver="0.11.0"
+
+start "Microsoft Azure Export for Terraform (aztfexport) $ver"
+
+# install Azure Terrafy
+cd /tmp
+curl -fL "https://github.com/Azure/aztfexport/releases/download/v${ver}/aztfexport_v${ver}_linux_amd64.zip" -o aztfexport.zip
+unzip aztfexport.zip
+mkdir -p  "$HOME/.local/bin"
+mv aztfexport "$HOME/.local/bin"
+rm aztfexport.zip
+
+end 'aztfexport' '--version'
