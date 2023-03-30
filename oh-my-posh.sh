58bfe2af74ae6c01557ba486c48220ef1928cf21
Adam Rush <adamrushuk@gmail.com>
2023-03-30 09:07:10 +0100

    Bumps oh-my-posh to 14.22.0

--- oh-my-posh.sh
+++ oh-my-posh.sh
@@ -5,7 +5,7 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 source "$DIR/.lib.sh"
 
 # https://github.com/JanDeDobbeleer/oh-my-posh3/releases
-ver="v8.6.0"
+ver="v14.22.0"
 
 start "oh-my-posh $ver"
 
@@ -13,4 +13,4 @@ start "oh-my-posh $ver"
 mkdir -p  "$HOME/.local/bin"
 wget -q "https://github.com/JanDeDobbeleer/oh-my-posh3/releases/download/${ver}/posh-linux-amd64" -O ~/.local/bin/oh-my-posh
 
-end 'oh-my-posh' '-version'
+end 'oh-my-posh' '--version'
