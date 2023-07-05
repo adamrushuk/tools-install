eac6884c3c3a4639cdb9c896ed3312a506283c48
Adam Rush <adamrushuk@gmail.com>
2023-07-05 15:40:16 +0100

    Adds homebrew install script

---
+++ homebrew.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap "echo 'error: Script failed: see failed command above'" ERR
+DIR=$(cd "$(dirname "$0")" && pwd)
+source "$DIR/.lib.sh"
+
+# install homebrew
+start 'homebrew'
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+end 'brew' '--version'
