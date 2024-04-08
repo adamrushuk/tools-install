a31de2924e2c11fc3f6caf977914a430465282a0
Adam Rush <adamrushuk@gmail.com>
2024-04-08 17:08:06 +0100

    Add WSL Utilities installation to misc.sh

--- misc.sh
+++ misc.sh
@@ -5,3 +5,8 @@ trap "echo 'error: Script failed: see failed command above'" ERR
 # Misc system utilities
 sudo apt-get update -y
 sudo apt-get install -y jq pv unzip tree
+
+# Install WSL Utilities - Azure CLIs can open browser windows for login
+sudo add-apt-repository ppa:wslutilities/wslu
+sudo apt update
+sudo apt install wslu
