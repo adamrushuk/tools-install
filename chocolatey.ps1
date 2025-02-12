7d39b11ba318fba2048dc61b7f6b021322fae2ac
Adam Rush <adamrushuk@gmail.com>
2025-02-12 18:23:14 +0000

    Update packages

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,53 +1,35 @@
 # List of Chocolatey packages to install
-7zip.install
+$packages = @(
+    "7zip.install",
+    # "cascadiacodepl",
+    # "docker",
+    # "docker-cli",
+    # "docker-compose",
+    # "rancher-desktop",
+    "Firefox",
+    "foxitreader",
+    "git.install",
+    "k9s",
+    # "microsoftazurestorageexplorer",
+    "mRemoteNG",
+    # "notepadplusplus.install",
+    # "openssl",
+    # "openvpn",
+    # "packer",
+    # "postman",
+    # "PowerShell",
+    "powertoys",
+    # "putty.install",
+    # "rufus",
+    # "speccy",
+    "sysinternals",
+    # "terraform",
+    # "treesizefree",
+    "velero",
+    "vlc.install"
+)
 
-
-cascadiacodepl
-
-docker
-docker-cli
-docker-compose
-docker-desktop
-
-# dotnetfx
-Firefox
-foxitreader
-# gh
-# git
-git.install
-# GoogleChrome
-# jq
-# jre8
-k9s
-
-# kind
-# kubectx
-# kubens
-# kubernetes-cli
-# kubernetes-helm
-# kubeval
-# kustomize
-# lockhunter
-microsoftazurestorageexplorer
-
-mRemoteNG
-
-notepadplusplus.install
-
-openssl
-openvpn
-packer
-postman
-PowerShell
-powertoys
-putty.install
-rufus
-
-speccy
-sysinternals
-terraform
-treesizefree
-
-velero
-vlc
-vlc.install
+# Install each package using Chocolatey
+foreach ($package in $packages) {
+    choco install $package -y
+}
