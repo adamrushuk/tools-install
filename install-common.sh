b2aada8e0bff2ad0eb3ada59b13683db4a17873c
Adam Rush <adam.rush103@iace.mod.gov.uk>
2026-01-13 10:39:40 +0000

    Fixed azure-cli

--- install-common.sh
+++ install-common.sh
@@ -7,6 +7,7 @@ source "$DIR/.lib.sh"
 sudo apt update -y && sudo apt upgrade -y
 
 echo -e "\e[34m»»» 🚀 \e[32mInstalling \e[33m'Common Tools'\e[32m ... \e[39m"
+~/tools/azure-cli.sh
 ~/tools/helm.sh
 ~/tools/powershell.sh
 ~/tools/powershell-modules.sh
@@ -16,7 +17,6 @@ echo -e "\e[34m»»» 🚀 \e[32mInstalling \e[33m'Common Tools'\e[32m ... \e[39
 ~/tools/velero.sh
 
 # TO FIX
-~/tools/azure-cli.sh # az aks install-cli currently getting blocked
 ~/tools/kubectl.sh
 ~/tools/kube-tools.sh # waiting to get kubectl working first
 ~/tools/misc.sh # are WSL Utilities still required so Azure CLIs can open browser windows for login?
